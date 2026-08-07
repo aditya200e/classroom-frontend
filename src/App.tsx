1bb898c9661dcf954bef3322e0188915d0ef3774
Aditya Kawade <adityakawadepvt@gmail.com>
2026-08-07 19:43:23 +0530

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -25,11 +25,14 @@ import { useNotificationProvider } from "./components/refine-ui/notification/use
 import { Toaster } from "./components/refine-ui/notification/toaster";
 import { ThemeProvider } from "./components/refine-ui/theme/theme-provider";
 import "./App.css";
+import Dashboard from "@/pages/dashboard.tsx";
+import {BookOpen, Home} from "lucide-react";
+import SubjectsList from "@/pages/subjects/list.tsx";
+import SubjectsCreate from "@/pages/subjects/Create.tsx";
 
 function App() {
   return (
     <BrowserRouter>
-      <GitHubBanner />
       <RefineKbarProvider>
         <ThemeProvider>
           <DevtoolsProvider>
@@ -42,9 +45,37 @@ function App() {
                 warnWhenUnsavedChanges: true,
                 projectId: "uGDbiB-hiOEaH-bs9DKw",
               }}
+              resources={[
+                {
+                  name: 'dashboard',
+                  list: '/',
+                  meta: {label:'Home', icon:<Home />}
+                },{
+                  name: 'subjects',
+                  list: '/subjects',
+                  create: '/subjects/create',
+                  meta: {label: 'Subjects', incon: <BookOpen />}
+
+                }
+              ]}
             >
               <Routes>
-                <Route index element={<WelcomePage />} />
+                <Route element={
+                  <Layout>
+                    <Outlet />
+                  </Layout>
+                } >
+                  <Route path='/' element={<Dashboard/>}/>
+
+                  <Route path={"subjects"} >
+                    <Route index element={<SubjectsList />}/>
+                    <Route path="create" element={<SubjectsCreate />} ></Route>
+                  </Route>
+
+                </Route>
+
+
+
               </Routes>
               <Toaster />
               <RefineKbar />
---
+++ src/pages/subjects/Create.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+
+const  SubjectsCreate = () => {
+    return (
+        <div>CreateSubject</div>
+    )
+}
+export default SubjectsCreate
