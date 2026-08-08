4d540a9310e66309284aee4d2ad58905d9a46232
Aditya Kawade <adityakawadepvt@gmail.com>
2026-08-08 17:47:25 +0530

    implement data provider

---
+++ src/constants/index.ts
@@ -0,0 +1,10 @@
+export const Departments = [
+    'CS' ,
+    'Math',
+    'English',
+];
+
+export const DEPARTMENT_OPTIONS = Departments.map((dept)=>({
+    value : dept,
+    label : dept,
+}));
